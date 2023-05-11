cac76552e8cc672f3b66fb11e3f468f1f50a10bd
Kunal Srinivasan <98067291+srinivasan-kunal@users.noreply.github.com>
2023-05-11 09:13:31 -0400

    Art registry application update

--- Kunal/app.py
+++ Kunal/app.py
@@ -4,6 +4,7 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
+from PIL import Image  
 
 from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 
@@ -11,13 +12,6 @@ load_dotenv()
 
 # Define and connect a new Web3 provider
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
-
-################################################################################
-# Contract Helper function:
-# 1. Loads the contract once using cache
-# 2. Connects to the contract using the contract address and ABI
-################################################################################
-
 
 @st.cache(allow_output_mutation=True)
 def load_contract():
@@ -41,11 +35,6 @@ def load_contract():
 # Load the contract
 contract = load_contract()
 
-################################################################################
-# Helper functions to pin files and json to Pinata
-################################################################################
-
-
 def pin_artwork(artwork_name, artwork_file):
     # Pin the file to IPFS with Pinata
     ipfs_file_hash = pin_file_to_ipfs(artwork_file.getvalue())
@@ -55,12 +44,14 @@ def pin_artwork(artwork_name, artwork_file):
         "name": artwork_name,
         "image": ipfs_file_hash
     }
+
     json_data = convert_data_to_json(token_json)
 
     # Pin the json to IPFS with Pinata
     json_ipfs_hash = pin_json_to_ipfs(json_data)
 
-    return json_ipfs_hash
+    return token_json, json_ipfs_hash
+    # return json_data
 
 
 def pin_appraisal_report(report_content):
@@ -78,37 +69,15 @@ st.markdown("---")
 # Register New Artwork
 ################################################################################
 st.markdown("## Register new Artwork")
-# Create the streamlit components required to get the following data from the user:
-# 1. Artwork name
-# 2. Artist name
-# 3. Initial appraisal value
+file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png"])
+
 artwork_name = st.text_input("Enter the name of the artwork")
 artist_name = st.text_input("Enter the artist name")
 initial_appraisal_value = st.text_input("Enter the initial appraisal amount")
-file = st.file_uploader("Upload Artwork", type=["jpg", "jpeg", "png"])
-# The uploader component lets users upload files and send them to Pinata. With this component, 
-# we can specify the types of files that we want to allow for upload. In this case, we want only 
-# digital images for the artwork. So we specify a list of types that consists of “jpg”, “jpeg”, 
-# “png”. The user can upload an image of one of these types, and we then call the 
-# pin_file_to_ipfs function with the file data. We do so by using the getvalue function to get 
-# the file data from the Streamlit file uploader component. The pin_file_to_ipfs function then 
-# sends the file to Pinata and returns the IPFS hash for that file. We can then use the hash as 
-# the URI for the tokens!
 
-# Create a button called "Register Artwork" that uses the contract's
-# registerArtwork function to register new artwork.
-# Display the receipt for the transaction on the webpage.
-# @TODO: YOUR CODE HERE!
 if st.button("Register Artwork"):
-    # st.write(address)
-    # Use the `pin_artwork` helper function to pin the file to IPFS
     artwork_ipfs_hash =  pin_artwork(artwork_name,file)
-    st.write(artwork_ipfs_hash)
-        
     artwork_uri = f"ipfs://{artwork_ipfs_hash}"
-
-
-
     tx_hash =contract.functions.registerArtwork(
         address,
         artwork_name,
@@ -120,65 +89,26 @@ if st.button("Register Artwork"):
     receipt=w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt")
     st.write(dict(receipt))
+
     st.write("You can view the pinned metadata file with the following IPFS Gateway Link")
     st.markdown(f"[Artwork IPFS Gateway Link](https://ipfs.io/ipfs/{artwork_ipfs_hash})")
-st.markdown("---")
-
-
-################################################################################
-# Appraise Art
-################################################################################
-st.markdown("## Appraise Artwork")
-tokens = contract.functions.totalSupply().call()
-token_id = st.selectbox("Choose an Art Token ID", list(range(tokens)))
-new_appraisal_value = st.text_input("Enter the new appraisal amount")
-appraisal_report_content = st.text_area("Enter details for the Appraisal Report")
-if st.button("Appraise Artwork"):
-
-    # Use Pinata to pin an appraisal report for the report URI
-    appraisal_report_ipfs_hash =  pin_appraisal_report(appraisal_report_content)
-    report_uri = f"ipfs://{appraisal_report_ipfs_hash}"
-
-    # Use the token_id and the report_uri to record the appraisal
-    tx_hash = contract.functions.newAppraisal(
-        token_id,
-        int(new_appraisal_value),
-        report_uri
-    ).transact({"from": w3.eth.accounts[0]})
-    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    st.write(receipt)
-st.markdown("---")
-
-################################################################################
-# Get Appraisals
-################################################################################
-st.markdown("## Get the appraisal report history")
-# Create a streamlit component that inputs a artwork token id from the user
-art_token_id=st.number_input("Artwork ID",value=0,step=1)
-if st.button("Get Appraisal Reports"):
-    # Create a filter that lists all of the Appraisal events for the token.
-    # @TODO: YOUR CODE HERE!
-    appraisal_filter = contract.events.Appraisal.createFilter(
-        fromBlock="0x0", argument_filters={"tokenId": art_token_id}
-    )
     
-    reports = appraisal_filter.get_all_entries()
+    location1=artwork_ipfs_hash[0]["image"]
+    location2=f"ipfs://{location1}"
 
-    if reports:
-        for report in reports:
-            report_dictionary = dict(report)
-            st.markdown("### Appraisal Report Event Log")
-            st.write(report_dictionary)
-            st.markdown("### Pinata IPFS Report URI")
-            report_uri = report_dictionary["args"]["reportURI"]
-            report_ipfs_hash = report_uri[7:]
-            st.markdown(
-                f"The report is located at the following URI: "
-                f"{report_uri}"
-            )
-            st.write("You can also view the report URI with the following ipfs gateway link")
-            st.markdown(f"[IPFS Gateway Link](https://ipfs.io/ipfs/{report_ipfs_hash})")
-            st.markdown("### Appraisal Event Details")
-            st.write(report_dictionary["args"])
-    else:
-        st.write("This artwork has no new appraisals")
+st.markdown("---")
+
+st.markdown("## Display an Art Token")
+
+tokens = contract.functions.balanceOf(address).call()
+st.write(f"This address owns {tokens} tokens")
+token_id = st.selectbox("Artwork Tokens", list(range(tokens)))
+if st.button("Display"):
+    # Use the contract's `ownerOf` function to get the art token owner
+    owner = contract.functions.ownerOf(token_id).call()
+    st.markdown("**The token address is**")
+    st.write(f"{owner}")
+    # Use the contract's `tokenURI` function to get the art token's URI
+    token_uri = contract.functions.tokenURI(token_id).call()
+    st.write(f"The tokenURI is {token_uri}")
+    st.image(file)
